70c303400334d2d8c16c49462e22ddf81dad6f56
anujsb <anujsbhuyar@gmail.com>
2024-10-15 10:05:03 +0530

    drizzle orm setup

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+const dbUrl = process.env.DATABASE_URL || "default_database_url"; // Replace with a suitable default value
+
+export default defineConfig({
+  schema: "./configs/schema.ts",
+  out: "./drizzle",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: dbUrl,
+  }
+});
